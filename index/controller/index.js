6d08fc81bc668348b2ed835df9b88536566c5240
zacking0712 <nldangkhoa0712@gmail.com>
2024-03-06 14:09:22 +0700

    Render Thành Công

--- index/controller/index.js
+++ index/controller/index.js
@@ -7,25 +7,52 @@ import { Product } from '../model/product.js';
 const service = new Service();
 let cart = [];
 
-function renderList(){
-  let content = '';
-  shoesList.forEach((ele) => {
-    content += ` 
-    <div class="col-lg-3 col-md-6">
-        <div class="card text-black h-100">
-            <div class="content-overlay"></div>
-            <img src=${ele.img} class="card-img" alt="Shoes Image" />
-            <div class="card-body">
-                <div class="text-center">
-                    <h5 class="card-title pt-3">${ele.name}</h5>
-                    <span class="text-muted mb-2">$${ele.shortDescription}</span>          
-                </div>    
-                <button type="button" class="btn btn-block w-50" onclick ="btnAddToCart('${ele.id}')">Buy now</button>
-            </div>
-        </div>
-    </div>`;
+function renderList() { 
+  let promise = axios({
+    url: '../../admin/data/sneaker.json',
+    method: "GET",
   });
-  getEle('shoesList').innerHTML = content;
-  console.log(content);
-};
+
+  promise
+    .then(function (res) {
+      console.log(res)
+      // Hiển thị giày
+      // let id_shoes = document.querySelectorAll('.shoes'); //Sai vì chưa có class .shoes nao tren html 
+      // Div chứa danh sách sản phẩm cần render
+      let divShoesList = document.querySelector('#shoesList')
+      console.log(divShoesList)
+      // Danh sách các sản phẩm từ API
+      let arrShoes = res.data.content
+      console.log(arrShoes)
+      let content=""
+      // Render danh sách giày từ API
+      for (const shoes of arrShoes) {
+        console.log(shoes);
+        content += `
+        <div class="col-lg-3 col-md-6">
+                  <div class="card text-black h-100">
+                       <div class="content-overlay"></div>
+                       <img src=${shoes.image} class="card-img" alt="Shoes Image" />
+                       <div class="card-body">
+                           <div class="text-center">
+                                 <h5 class="card-title pt-3">${shoes.name}</h5>
+                               <span class="text-muted mb-2">$${shoes.shortDescription}</span>          
+                           </div>    
+                           <button type="button" class="btn btn-dark w-100 my-3" onclick ="btnAddToCart('${shoes.id}')">Mua ngay</button>
+                       </div>
+                   </div>
+               </div>
+        
+      </div>`
+      }
+      divShoesList.innerHTML=content
+
+
+    })
+    .catch(function (error) {
+      console.log(error);
+    });
+}
+
+renderList();
 
